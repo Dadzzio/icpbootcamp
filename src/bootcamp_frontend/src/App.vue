49c0b1494b094d5e078e7ef1ab980850e705325d
Dadzzio <szymon09payerhin@gmail.com>
2024-06-24 22:08:55 +0200

    first commit

---
+++ src/bootcamp_frontend/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <main>
+    <img src="/logo2.svg" alt="DFINITY logo">
+
+    <h1><i>Dzień dobry witam państwa w moim pierwszym projekcie na BOOTCAMP!</i></h1>
+    <br>
+    <button @click="podbij">Kliknij mnie!!!!</button>
+    <p>{{ licznik }}</p>
+  </main>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      licznik:0
+    }
+  },
+  methods: {
+    podbij() {
+      this.licznik++
+    }
+  }
+}
+</script>
